b200ad814b569b1d46b07c3a762c9532762b9f74
ashdownbadd <u.randalljay00@gmail.com>
2026-09-01 08:23:39 +0800

    UI FIX

--- resources/views/layouts/app.php
+++ resources/views/layouts/app.php
@@ -16,7 +16,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 
     <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Syne:wght@400;500;600;700;800&family=IBM+Plex+Mono:wght@400;500&display=swap"
+        href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Inter:wght@400;500;600;700;800&display=swap"
         rel="stylesheet">
 
     <link
